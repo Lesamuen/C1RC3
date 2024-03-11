6f9cbd6c9b4d9f4f0c6e8f074f70fef936fbed90
Samuel Nguyen <lesamuengu@gmail.com>
2024-03-11 02:43:34 -0700

    Align format of dialogue

--- modules/admin.py
+++ modules/admin.py
@@ -14,20 +14,20 @@ async def admin_shutdown(context: ApplicationContext):
     """Adds the command /admin shutdown"""
 
     if context.author.id in perms["admin"]:
-        await context.respond("\"Permission granted. Shutdown protocol activated. Booting down...\"")
+        await context.respond("`\"Permission granted. Shutdown protocol activated. Booting down...\"`")
         log(get_time() + " >> Admin " + str(context.author) + " externally shut down C1RC3 from [" + str(context.guild) + "], [" + str(context.channel) + "]")
         await bot_client.close()
         quit()
     else:
-        await context.respond("\"Permission denied. You have no administrator privilege.\"")
+        await context.respond("`\"Permission denied. You have no administrator privilege.\"`")
         log(get_time() + " >> " + str(context.author) + " permission denied in [" + str(context.guild) + "], [" + str(context.channel) + "]")
 
-@bot_client.slash_command(name = "scan", description = "Scans the user, and internalizes key information.", guild_ids = guilds, guild_only = True)
+@bot_client.slash_command(name = "scan", description = "Scans the user, just for debugging", guild_ids = guilds, guild_only = True)
 async def scan(context: ApplicationContext):
     """Adds the command /scan"""
 
     await context.respond(".", ephemeral = True, delete_after = 0)
-    await context.channel.send("\"Scanning protocol activated. Please stand still.\"\n*C1RC3's eyes seem to glow a bit brighter, before a bright red cone of light shoots out and hovers from the top of your body to the very floor.*\n\"Scanning protocol complete. Your soul value is "
-         + str(context.author.id) + ". C1RC3 and the Casino by extension thanks you for your contribution.\"")
+    await context.channel.send("`\"Scanning protocol activated. Please stand still.\"`\n*C1RC3's eyes seem to glow a bit brighter, before a bright red cone of light shoots out and hovers from the top of your body to the very floor.*\n`\"Scanning protocol complete. Your soul value is "
+         + str(context.author.id) + ". C1RC3 and the Casino by extension thanks you for your contribution.\"`")
     log(get_time() + " >> " + str(context.author) + " scanned themselves in [" + str(context.guild) + "], [" + str(context.channel) + "]")
     log("id: " + str(context.author.id) + "\nname: " + str(context.author))
